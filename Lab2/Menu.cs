340a7dea2ceb61044cc1b75ac98055132212b508
eriksturen <eriksturen@gmail.com>
2022-09-22 17:23:11 +0200

    Menu is now working with arrows and so on. Back/Exit function in place.
    A bit irritating to have to do switch statements for each and every menu so that should be cleared up but good progress.

---
+++ Lab2/Menu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Menu
+    {
+        public int SelectedIndex { get; set; }
+        public string[] Options { get; set; }
+        public string Prompt { get; set; }
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+        }
+
+        // function to display whatever options is current 
+        // later going to be product list and settings and customer cart and so on
+        private void DisplayOptions()
+        {
+            Console.WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string CurrentOption = Options[i];
+                string prefix;
+
+                if (i == SelectedIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine($"{prefix} << {CurrentOption} >>");
+            }
+            Console.ResetColor();
+        }
+
+        public int Run()
+        {
+            // function updates everything drawn on screen every time user presses Up/Down
+            // functionality is essentially the same as Snake, although maybe more simple really
+            // it cycles through the provided array of options ;
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+
+                keyPressed = Console.ReadKey().Key;
+                // update SelectedIndex - increased or decreased based on up/down arrow
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    // these two if statements makes sure that the menu cyces back to start/end
+                    // instead of going off the screen
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+
+
+            return SelectedIndex;
+        }
+
+
+    }
+}
---
+++ Lab2/Shop.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Shop
+    {
+
+        string prompt = @"
+    ██╗  ██╗██╗   ██╗███╗   ██╗██████╗ ███████╗██████╗  ██████╗ ██████╗ ████████╗       ██╗        ██████╗ ██████╗ 
+    ██║  ██║██║   ██║████╗  ██║██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝       ██║       ██╔════╝██╔═══██╗
+    ███████║██║   ██║██╔██╗ ██║██║  ██║███████╗██████╔╝██║   ██║██████╔╝   ██║       ████████╗    ██║     ██║   ██║
+    ██╔══██║██║   ██║██║╚██╗██║██║  ██║╚════██║██╔═══╝ ██║   ██║██╔══██╗   ██║       ██╔═██╔═╝    ██║     ██║   ██║
+    ██║  ██║╚██████╔╝██║ ╚████║██████╔╝███████║██║     ╚██████╔╝██║  ██║   ██║       ██████║      ╚██████╗╚██████╔╝
+    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝       ╚═════╝       ╚═════╝ ╚═════╝ 
+                                                                                                                   
+    Välkommen till Hundsport & Co!
+    (Välj alternativ nedan med hjälp av piltangenterna och enter.)
+    ";
+
+        public void Start()
+        {
+            Console.Title = "Hundsport & Co";
+            RunMainMenu();
+
+        }
+
+        private void RunMainMenu()
+        {
+            string[] options = { "Mat", "Leksaker", "Koppel, halsband och selar", "Kassa", "Kundvagn", "Avsluta" };
+            Menu mainMenu = new Menu(prompt, options);
+            // tutorial makes this be saved as a variable. It is right now not strictly needed
+            int selectedIndex = mainMenu.Run();
+
+
+            // having to do a switch statement for each new menu is irritating - better way should be possible 
+            switch (selectedIndex)
+            {
+                case 0:
+                    Mat();
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    Tillbaka();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Tillbaka()
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        private void Mat()
+        {
+            string[] options = { "Standardt extra", "Royal Canin", "Köttfärs på tunna", "Tillbaka" };
+            Menu matMenu = new Menu(prompt, options);
+            int selectedIndex = matMenu.Run();
+            if (selectedIndex == options.Length-1)
+            {
+                RunMainMenu();
+            }
+        }
+    }
+}
